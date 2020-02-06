5626bb8604d4f891caab3191d1a43d395ceb27cd
Rozerer <nazrockkinhem@gmail.com>
2020-02-06 17:13:42 +0200

    async
    async get_Photo_Link

--- Tsumi.py
+++ Tsumi.py
@@ -3,6 +3,7 @@ import time
 import telebot
 import json
 import hashlib
+import multiprocessing
 from selenium import webdriver
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from selenium.webdriver.common.by import By
@@ -10,12 +11,11 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.firefox.options import Options
 
-
 startUrl = 'https://www.instagram.com/accounts/login/'
 
 token = ''
 
-bot = telebot.TeleBot(token)
+bot = telebot.AsyncTeleBot(token)
 
 with open('/home/user1/Tsumi/TsumiUsersDB.json') as json_file:
 #with open('TsumiUsersDB.json') as json_file:
@@ -25,10 +25,7 @@ with open('/home/user1/Tsumi/TsumiUsersDB.json') as json_file:
 options = Options()
 options.headless = True
 
-login = ''
-password = ''
-
-def get_Photo_Link(url, log, pas):
+def get_Photo_Link(id, url, log, pas):
 
     #driver = webdriver.Firefox()
     driver = webdriver.Firefox(options=options,executable_path='/home/user1/Tsumi/geckodriver')
@@ -83,6 +80,8 @@ def get_Photo_Link(url, log, pas):
 
     driver.quit()
 
+    bot.send_message(id,res)
+
     return res
 
 
@@ -90,6 +89,7 @@ def get_login(message):
     global login
     login = message.text
     bot.send_message(message.from_user.id, 'Enter your Instagram password')
+    
     bot.register_next_step_handler(message, get_password)
 
 
@@ -99,10 +99,12 @@ def get_password(message):
     password = message.text
     reg_data = {'id': message.from_user.id,
                 'Login': login, 'Password': password}
+    print(message.from_user.id)
     for user in data:
         if user['id'] == message.from_user.id:
             user['Login'] = login
             user['Password'] = password
+
             break
     else:
         data.append(reg_data)
@@ -114,7 +116,7 @@ def get_password(message):
 def get_text_messages(message):
     if message.text == "Привет":
         bot.send_message(message.from_user.id,
-                         "Привет, чем я могу тебе помочь?")
+                         "Hey, how can I help you?")
     elif message.text == "/start":
         bot.send_message(message.from_user.id, "Hello, my name is Tsumi <3")
         bot.send_message(message.from_user.id,
@@ -126,15 +128,12 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "/reg - your account data")
     else:
         for user in data:
-            if user['id'] == message.from_user.id:
-                login = user['Login']
-                password = user['Password']
-                bot.send_message(message.from_user.id, get_Photo_Link(
-                    message.text, login, password))
+            if user['id'] == message.from_user.id:   
+                t = multiprocessing.Process(target=get_Photo_Link, args=(message.from_user.id, message.text, user['Login'],user['Password'],))
+                t.run()
                 break
-            else:
-                bot.send_message(message.from_user.id,
-                                 "Please register first. Use /reg")
-
+        else:
+            bot.send_message(message.from_user.id,
+            "Please register first. Use /reg")
 
 bot.polling(none_stop=True, interval=0)
